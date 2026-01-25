8b28ccb437104ed5cc109bb4aac105ede2d71d07
Borna Šoštarić <sostaric.borna.dev@hotmail.com>
2026-01-25 20:24:49 +0100

    v0.1.0

--- src/ezbuild/__init__.py
+++ src/ezbuild/__init__.py
@@ -3,7 +3,7 @@ from .environment import Environment, Program
 from .language import Language
 from .python_environment import PythonEnvironment
 
-__version__ = "0.0.0"
+__version__ = "0.1.0"
 
 __all__ = [
     "CompileCommand",
